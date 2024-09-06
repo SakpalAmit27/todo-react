fb239a23bccb12d72585afd201df00e6e1c14397
Sakpal Amit <amitsakpal744@gmail.com>
2024-09-06 18:33:44 +0530

    css and basic plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react'
 import '../src/styles.css'
 
 const App = () => {
+
+  
   return (
     <div>
       <form className='new-item-form'>
         <div className='form-row'>
-          <label>new item</label>
+          <label htmlFor='item'>new item</label>
           <input type="text" id='item' />
         </div>
+
+        <button className="btn">Add</button>
       </form>
+      <h1 className='header'>Todo</h1>
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" />
+          item 1
+        </label>
+        <button className='btn btn-danger'>Delete</button>
+      </li>
+
+      <li>
+        <label htmlFor="">
+          <input type="checkbox" />
+          item 1
+        </label>
+        <button className='btn btn-danger'>Delete</button>
+      </li>
     </div>
+
   )
 }
 
