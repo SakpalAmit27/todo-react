e15820c30280adfe2cca7b0f4f6e3d0d41b38d17
Sakpal Amit <amitsakpal744@gmail.com>
2024-09-06 18:51:04 +0530

    preventing changing pages while submitting

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import '../src/styles.css'
 
 const App = () => {
 
+  const [newItem , setNewItem] = useState("")
+
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+  }
   
   return (
     <div>
-      <form className='new-item-form'>
+      <form className='new-item-form' onSubmit={handleSubmit}>
         <div className='form-row'>
           <label htmlFor='item'>new item</label>
-          <input type="text" id='item' />
+          <input type="text" id='item' value={newItem} onChange={e => setNewItem(e.target.value)}  />
         </div>
 
         <button className="btn">Add</button>
@@ -17,7 +25,7 @@ const App = () => {
       <h1 className='header'>Todo</h1>
       <li>
         <label htmlFor="">
-          <input type="checkbox" />
+          <input type="checkbox"  />
           item 1
         </label>
         <button className='btn btn-danger'>Delete</button>
